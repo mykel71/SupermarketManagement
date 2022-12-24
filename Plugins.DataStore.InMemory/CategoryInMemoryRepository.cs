24ba4820840da95c88cbcb0ddeedcbca6fa2b257
Michael Shepherd <94452225+mykel71@users.noreply.github.com>
2022-12-24 22:50:06 +0200

    View Categories

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using CoreBusiness;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+        public CategoryInMemoryRepository()
+        {
+            //Add some default data
+            categories = new List<Category>()
+            {
+                new Category {CategoryId = 1, Name = "Beverages", Description = "Beverage"},
+                new Category {CategoryId = 2, Name = "Bakery", Description = "Bakery"},
+                new Category {CategoryId = 3, Name = "Meat", Description = "Meat"}
+            };
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            
+        }
+    }
+}
